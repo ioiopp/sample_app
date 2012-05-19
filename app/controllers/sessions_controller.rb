5c4f460af5fb396b7e0740d05090d8a54f40ceb7
Chaoran SONG <chaoran.song@gmail.com>
2012-05-20 01:34:43 +0200

    refector sign in. fixed flash.now problem

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
       redirect_to user
     else
       # Create an error message and re-render the signin form.
-      flash[:error] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
