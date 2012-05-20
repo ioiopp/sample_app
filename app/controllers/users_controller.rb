2b7742c334c8b7fa8c6fccc0f2ba082ea1ec73f6
Chaoran SONG <chaoran.song@gmail.com>
2012-05-20 11:30:42 +0200

    authorization implemented. non authorized user cannot edit others' profiles. friendly forward implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -18,4 +21,39 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    # @user = current_user# why not current_user?
+    # same effect, but the Spec test will not pass since it does not start from log in
+    # secondly, @user= User.find(params[:id]) may not be equal to the user logged in!!!
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+
+  end
+
+  private
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please sign in."
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+
 end
